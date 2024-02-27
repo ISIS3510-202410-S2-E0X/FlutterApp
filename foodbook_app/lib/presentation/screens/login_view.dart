0b772a30a96808a2c642f22a427a5fb0dfa986ed
JuanCa <j.fallag@uniandes.edu.co>
2024-02-27 14:56:24 -0500

    feature: Login UI completion.

---
+++ foodbook_app/lib/presentation/screens/login_view.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class login_view extends StatelessWidget {
+  const login_view({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 24),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0), // Adjust the value as needed
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'foodbook',
+                  style: GoogleFonts.archivoBlack(
+                    fontSize: 48,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Spacer(),
+            
+            Image.asset(
+              'lib/presentation/images/toasty.png',
+              height: 300, // Set the image height
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Where good people find good food.',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[700],
+              ),
+            ),
+            Spacer(),
+            // This would be your login or sign up button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 0, 120, 218), // background (button) color
+                  onPrimary: Colors.white, // foreground (text) color
+                  minimumSize: Size(double.infinity, 50), // set the size
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), // set the border radius
+                  ),
+                ),
+                onPressed: () {
+                  // Implement your login functionality
+                  print('Continue with Google');
+                },
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'lib/presentation/images/guglu.png',
+                        height: 24, // Set the image height
+                      ),
+                      SizedBox(width: 8), // Add some spacing between the image and text
+                      Text('Continue with Google'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 32),
+          ],
+        ),
+      ),
+    );
+  }
+}
+  
