204939e3b754b8358dd02afb1e2aff0f5351b1bf
JuanCa <j.fallag@uniandes.edu.co>
2024-03-08 15:28:10 -0500

    feature: login with google-firebase completion and UI adaptability creation

--- foodbook_app/lib/presentation/screens/signin_view.dart
+++ foodbook_app/lib/presentation/screens/signin_view.dart
@@ -4,15 +4,12 @@ import 'package:foodbook_app/bloc/auth_bloc.dart';
 import 'package:foodbook_app/presentation/screens/login_confirm.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-// Import your SignInBloc here
-
 class SignInView extends StatefulWidget {
   @override
   _SignInViewState createState() => _SignInViewState();
 }
 
 class _SignInViewState extends State<SignInView> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,105 +17,92 @@ class _SignInViewState extends State<SignInView> {
       body: Center(
         child: BlocBuilder<AuthBloc, AuthState>(
           builder: (context, state) {
-            if (state == Loading) {
+            if (state is Loading) {
               return const CircularProgressIndicator();
             } else {
-              return Column( 
-                children: [
-                SizedBox(height: 24),
-                Padding(
-                padding: const EdgeInsets.only(left: 16.0), // Adjust the value as needed
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    'foodbook',
-                    style: GoogleFonts.archivoBlack(
-                      fontSize: 48,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ),
-              Spacer(),
-              Image.asset(
-                'lib/presentation/images/toasty.png',
-                height: 300, // Set the image height
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Where good people find good food.',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey[700],
-                ),
-              ),
-              const Spacer(),
-              Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 70),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white, 
-                  backgroundColor: const Color.fromARGB(255, 0, 140, 255), // foreground (text) color
-                  minimumSize: const Size(double.infinity, 50), // set the size
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // set the border radius
-                  ),
-                ),
-                onPressed: () {
-                  BlocProvider.of<AuthBloc>(context).add(GoogleSignInRequested());
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginConfirmPage()),
-                  );
-                },
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start, // Align content to the left
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(6),
-                        child: Image.asset(
-                          'lib/presentation/images/google2.jpeg',
-                          height: 30, // Set the image height
-                        ),
-                      ),
-                      const SizedBox(width: 10), // Add some spacing between the image and text
-                      const Center(
-                        child: Text('Continue with Google'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 32),
-                ],
-            );
+              return buildSignInContent(context);
             }
           },
         ),
       ),
     );
   }
-}
-class LoginBtn extends StatelessWidget {
-  const LoginBtn({super.key});
-  @override
-  Widget build(BuildContext context) {
+
+  Widget buildSignInContent(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(height: screenSize.height * 0.05),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.05),
+          child: Text(
+            'foodbook',
+            style: GoogleFonts.archivoBlack(
+              fontSize: screenSize.width * 0.13,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        const Spacer(),
+        Image.asset(
+          'lib/presentation/images/toasty.png',
+          height: screenSize.height * 0.45,
+        ),
+        SizedBox(height: screenSize.height * 0.1),
+        Text(
+          'Where good people find good food.',
+          style: TextStyle(
+            fontSize: screenSize.width * 0.05,
+            color: Colors.grey[700],
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Spacer(),
+        buildGoogleSignInButton(context),
+        SizedBox(height: screenSize.height * 0.05),
+      ],
+    );
+  }
+
+  Widget buildGoogleSignInButton(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
     return Padding(
-      padding: EdgeInsetsGeometry.infinity,
-      child: OutlinedButton(
-        style: OutlinedButton.styleFrom(
-            side: const BorderSide(color: Colors.blue, width: 1),
-            minimumSize: const Size(double.infinity, 54),
-            backgroundColor: Colors.blue[50]),
+      padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.1),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: const Color.fromARGB(255, 0, 140, 255),
+          minimumSize: Size(double.infinity, screenSize.height * 0.07),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(screenSize.width * 0.05),
+          ),
+        ),
         onPressed: () {
-          BlocProvider.of<AuthBloc>(context)
-              .add(GoogleSignInRequested());
+          BlocProvider.of<AuthBloc>(context).add(GoogleSignInRequested());
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginConfirmPage()),
+          );
         },
-        child: const Text(
-          'Login',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(screenSize.width * 0.03),
+              child: Image.asset(
+                'lib/presentation/images/google2.jpeg',
+                height: screenSize.height * 0.04,
+              ),
+            ),
+            SizedBox(width: screenSize.width * 0.02),
+            Text(
+              'Continue with Google',
+              style: TextStyle(fontSize: screenSize.width * 0.04),
+            ),
+          ],
         ),
       ),
     );
