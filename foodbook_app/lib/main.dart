1b89421e7ae05422482a5104006b57b366b034d6
Juan Camilo Falla <78000303+JuanCamFalla@users.noreply.github.com>
2024-03-12 13:29:55 -0500

    Feature/3-fu-authentication-with-backend-google-auth (#33)
    * Integrated Firebase for the login UI. The application compiles and connects to firebase while displaying the login UI
    
    * Add Firebase authentication and update dependencies. State management and navigation pending
    
    * Login Functionality complete with the implementation of Firebase google login. Service adapter and Repository paterns are included
    
    * feature: login with google-firebase completion and UI adaptability creation

--- foodbook_app/lib/main.dart
+++ foodbook_app/lib/main.dart
@@ -1,39 +1,39 @@
 import 'package:flutter/material.dart';
-import 'presentation/views/login_view/login_view.dart';
-void main() {
-  runApp(MyApp());
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodbook_app/bloc/auth_bloc.dart';
+import 'package:foodbook_app/data/repository/auth_repository.dart';
+import 'package:foodbook_app/presentation/views/login_view/signin_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FoodBook App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const login_view(), // Assuming LoginView is your starting view
+    return RepositoryProvider(
+      create: (context) => AuthRepository(),
+      child: BlocProvider(
+        create: (context) => AuthBloc(
+          authRepository: RepositoryProvider.of<AuthRepository>(context),
+          ),
+        child: MaterialApp(
+          title: 'FoodBook',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: SignInView(),
+          ),
+        ),
+        
     );
   }
 }
-
-
-// class MyApp extends StatelessWidget {
-//   MyApp({super.key});
-
-//   final ReviewBloc reviewBloc = ReviewBloc(ReviewRepository());
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'MiCaserrito Reviews',
-//       home: BlocProvider(
-//         create: (context) => reviewBloc,
-//         child: ReviewList(),
-//       ),
-//     );
-//   }
-// }
 
