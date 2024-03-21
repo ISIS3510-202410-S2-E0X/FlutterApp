aed4ee4c4ff70feec7543b50269328d100103289
Alejandro1400 <a.salgadom@uniandes.edu.co>
2024-03-21 00:39:58 -0500

    fix: Fixed Images URI display

--- foodbook_app/lib/presentation/views/restaurant_view/browse_view.dart
+++ foodbook_app/lib/presentation/views/restaurant_view/browse_view.dart
@@ -29,7 +29,7 @@ class BrowseView extends StatelessWidget {
           ),
           // Add the FilterBar widget to the AppBar
           actions: [
-            FilterBar(),
+            //FilterBar(),
           ],
           elevation: 0, // Remove shadow
         ),
