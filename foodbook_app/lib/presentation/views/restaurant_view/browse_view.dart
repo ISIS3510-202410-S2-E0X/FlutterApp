50d8571ced6b05519b08ce16ed46906bdb2c4296
Nicolás Klopstock Triana <77993013+nklopstock611@users.noreply.github.com>
2024-03-20 23:32:28 -0500

    bug-fix: Android Back-button desabled when in BrowseView, ForYouView or BookMarksView (#67)

--- foodbook_app/lib/presentation/views/restaurant_view/browse_view.dart
+++ foodbook_app/lib/presentation/views/restaurant_view/browse_view.dart
@@ -14,75 +14,78 @@ class BrowseView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.white, // Set AppBar background to white
-        title: const Text(
-          'Browse',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.black, // Title color
-          ),
-        ),
-        // Add the FilterBar widget to the AppBar
-        actions: [
-          FilterBar(),
-        ],
-        elevation: 0, // Remove shadow
-      ),
-      backgroundColor: Colors.grey[200], // Set the background color to grey
-      body: Column(
-        children: [
-          RestaurantSearchBar(browseBloc: BlocProvider.of<BrowseBloc>(context)),
-          Divider(
-            height: 1, // Height of the divider line
-            color: Colors.grey[300], // Color of the divider line
-          ),
-          Expanded(
-            child: BlocBuilder<BrowseBloc, BrowseState>(
-              builder: (context, state) {
-                if (state is RestaurantsLoadInProgress) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (state is RestaurantsLoadSuccess) {
-                  return ListView.builder(
-                    itemCount: state.restaurants.length,
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          // Navigate to another view when the restaurant card is clicked
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => SpotDetail(restaurant: state.restaurants[index]),
-                            ),
-                          );
-                        },
-                        child: RestaurantCard(restaurant: state.restaurants[index]),
-                      );
-                    }
-                  );
-                } else if (state is RestaurantsLoadFailure) {
-                  return const Center(child: Text('Failed to load restaurants'));
-                }
-                // Si el estado inicial es RestaurantsInitial o cualquier otro estado no esperado
-                return const Center(child: Text('Start browsing by applying some filters!'));
-              },
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white, // Set AppBar background to white
+          title: const Text(
+            'Browse',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black, // Title color
             ),
           ),
-        ],
-      ),
-      bottomNavigationBar: CustomNavigationBar(
-        selectedIndex: 0, // Set the selected index to 1
-        onItemTapped: (int index) {
-          // Handle navigation to different views
-          if (index == 1) {
-            Navigator.pushNamed(context, 'package:foodbook_app/presentation/views/restaurant_views/login_view.dart');
-          } else if (index == 2) {
-            Navigator.pushNamed(context, '/bookmarks');
-          }
-        },
-      ),
+          // Add the FilterBar widget to the AppBar
+          actions: [
+            FilterBar(),
+          ],
+          elevation: 0, // Remove shadow
+        ),
+        backgroundColor: Colors.grey[200], // Set the background color to grey
+        body: Column(
+          children: [
+            RestaurantSearchBar(browseBloc: BlocProvider.of<BrowseBloc>(context)),
+            Divider(
+              height: 1, // Height of the divider line
+              color: Colors.grey[300], // Color of the divider line
+            ),
+            Expanded(
+              child: BlocBuilder<BrowseBloc, BrowseState>(
+                builder: (context, state) {
+                  if (state is RestaurantsLoadInProgress) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state is RestaurantsLoadSuccess) {
+                    return ListView.builder(
+                      itemCount: state.restaurants.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            // Navigate to another view when the restaurant card is clicked
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SpotDetail(restaurant: state.restaurants[index]),
+                              ),
+                            );
+                          },
+                          child: RestaurantCard(restaurant: state.restaurants[index]),
+                        );
+                      }
+                    );
+                  } else if (state is RestaurantsLoadFailure) {
+                    return const Center(child: Text('Failed to load restaurants'));
+                  }
+                  // Si el estado inicial es RestaurantsInitial o cualquier otro estado no esperado
+                  return const Center(child: Text('Start browsing by applying some filters!'));
+                },
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: CustomNavigationBar(
+          selectedIndex: 0, // Set the selected index to 1
+          onItemTapped: (int index) {
+            // Handle navigation to different views
+            if (index == 1) {
+              Navigator.pushNamed(context, 'package:foodbook_app/presentation/views/restaurant_views/login_view.dart');
+            } else if (index == 2) {
+              Navigator.pushNamed(context, '/bookmarks');
+            }
+          },
+        ),
+      )
     );
   }
 }
