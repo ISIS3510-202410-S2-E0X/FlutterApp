aa230f8c0991dddc74e2f8c804066d115025a8af
JuanCa <j.fallag@uniandes.edu.co>
2024-04-13 15:45:56 -0500

    fix: added a DAO file in order to access the file manager of the android device. this allows the user to save the image and retrieve the image

---
+++ foodbook_app/lib/data/data_access_objects/file_manager_dao.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+class FileManagerDAO {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile () async {
+    final path = await _localPath;
+    // You can customize the file name here as per your requirement
+    return File('$path/image.png');
+  }
+
+  Future<void> saveImage(File imageFile) async {
+    final file = await _localFile;
+    // Write the file
+    await file.writeAsBytes(imageFile.readAsBytesSync());
+  }
+
+  Future<File?> getImage() async {
+  try {
+    final file = await _localFile;
+    // Check if the file exists
+    if (await file.exists()) {
+      return file;
+    } else {
+      print("Image file does not exist.");
+      return null;
+    }
+  } catch (e) {
+    print("Error while getting image: $e");
+    return null;
+  }
+}
+
+}
