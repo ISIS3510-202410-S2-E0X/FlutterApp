db81d80aa5fbd0772d41e65630a9392b2799a0ff
Alejandro1400 <a.salgadom@uniandes.edu.co>
2024-04-21 20:07:59 -0500

    Feat: Implemented sharedpreferences logic

---
+++ foodbook_app/lib/data/repositories/bookmark_manager.dart
@@ -0,0 +1,31 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class BookmarkManager {
+  static const String _bookmarksKey = 'bookmarks';
+
+  Future<void> bookmarkRestaurant(String restaurantName) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> bookmarks = prefs.getStringList(_bookmarksKey) ?? [];
+    if (!bookmarks.contains(restaurantName)) {
+      bookmarks.add(restaurantName);
+      await prefs.setStringList(_bookmarksKey, bookmarks);
+    }
+  }
+
+  Future<void> unbookmarkRestaurant(String restaurantName) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> bookmarks = prefs.getStringList(_bookmarksKey) ?? [];
+    bookmarks.remove(restaurantName);
+    await prefs.setStringList(_bookmarksKey, bookmarks);
+  }
+
+  Future<List<String>> getBookmarkedRestaurants() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList(_bookmarksKey) ?? [];
+  }
+
+  Future<bool> isBookmarked(String restaurantName) async {
+    final List<String> bookmarks = await getBookmarkedRestaurants();
+    return bookmarks.contains(restaurantName);
+  }
+}
