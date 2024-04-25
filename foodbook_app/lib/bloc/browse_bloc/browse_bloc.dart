49f7f388f9313541476e92f94a9063d7e40c5c04
JuanCa <j.fallag@uniandes.edu.co>
2024-04-25 08:43:43 -0500

    removed dead code from BrowseBloc. Mostly prints

--- foodbook_app/lib/bloc/browse_bloc/browse_bloc.dart
+++ foodbook_app/lib/bloc/browse_bloc/browse_bloc.dart
@@ -27,9 +27,7 @@ class BrowseBloc extends Bloc<BrowseEvent, BrowseState> {
     emit(RestaurantsLoadInProgress());
     
     try {
-      print("Fetching restaurants");
       final restaurants = await restaurantRepository.fetchRestaurants();
-      print("Restaurants size: ${restaurants.length}");
       if (restaurants.isEmpty) {
         final cachedRests = await restaurantRepository.fetchRestaurantsFromCache();
         if (cachedRests.isEmpty) {
@@ -37,7 +35,7 @@ class BrowseBloc extends Bloc<BrowseEvent, BrowseState> {
         }
         
         emit(RestaurantsLoadSuccess(cachedRests));
-        print("Fetched restaurants from cache");
+
       }
       if (restaurants.isNotEmpty) {
         emit(RestaurantsLoadSuccess(restaurants));
