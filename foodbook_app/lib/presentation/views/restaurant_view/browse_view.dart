a016267d5759f86c256b46c6801b59e986c92356
Alejandro  Salgado <78111596+Alejandro1400@users.noreply.github.com>
2024-05-19 13:26:36 -0500

    feat: Implemented navigation to settings page view

--- foodbook_app/lib/presentation/views/restaurant_view/browse_view.dart
+++ foodbook_app/lib/presentation/views/restaurant_view/browse_view.dart
@@ -152,31 +152,32 @@ class _BrowseViewState extends State<BrowseView> {
               appBar: AppBar(
                 automaticallyImplyLeading: false,
                 backgroundColor: Colors.white, // Set AppBar background to white
-                title: Row(
-                  children: [
-                    const Text(
-                      'Browse',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black, // Title color
-                      ),
+                title: Center(
+                  child: Text(
+                    'Browse',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black, // Title color
                     ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.58, // Adjust the width as needed
-                      height: kToolbarHeight, // Constrain the height of the SearchPage2 widget
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: SearchPage2(browseBloc: BlocProvider.of<BrowseBloc>(context)),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-                elevation: 0, // Remove shadow
+                leading: IconButton(
+                  icon: Icon(Icons.settings, color: Colors.black), // Settings icon
+                  onPressed: () {
+                    context.read<SettingsBloc>().add(LoadSettings()); 
+
+                    // Navigate to the settings page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SettingsPage()), 
+                    );
+                  },
+                ),
                 actions: [
                   IconButton(
-                    icon: const Icon(Icons.account_circle, color:  Color.fromARGB(255, 0, 140, 255)), // Profile icon
+                    icon: const Icon(Icons.account_circle, color: Color.fromARGB(255, 0, 140, 255)), // Profile icon
                     onPressed: () {
-                      //Navigate to the profile view
+                      // Navigate to the profile view
                       context.read<UserBloc>().add(GetCurrentUser());
                       Navigator.push(
                         context,
@@ -185,6 +186,18 @@ class _BrowseViewState extends State<BrowseView> {
                     },
                   ),
                 ],
+                elevation: 0, // Remove shadow
+                bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(kToolbarHeight), // Adjust the height for the search bar
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.white, // Maintain the same color as AppBar
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: SearchPage2(browseBloc: BlocProvider.of<BrowseBloc>(context)),
+                    ),
+                  ),
+                ),
               ),
               backgroundColor: Colors.grey[200], // Set the background color to grey
               body: BlocBuilder<BookmarkInternetViewBloc, BookmarkInternetViewState>(
@@ -267,87 +280,3 @@ class _BrowseViewState extends State<BrowseView> {
     );
   }
 }
-
-
-
-// class BrowseView extends StatelessWidget {
-//   BrowseView({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return PopScope(
-//       canPop: false,
-//       child: Scaffold(
-//         appBar: AppBar(
-//           automaticallyImplyLeading: false,
-//           backgroundColor: Colors.white, // Set AppBar background to white
-//           title: const Text(
-//             'Browse',
-//             style: TextStyle(
-//               fontWeight: FontWeight.bold,
-//               color: Colors.black, // Title color
-//             ),
-//           ),
-//           // Add the FilterBar widget to the AppBar
-//           actions: [
-//             //FilterBar(),
-//           ],
-//           elevation: 0, // Remove shadow
-//         ),
-//         backgroundColor: Colors.grey[200], // Set the background color to grey
-//         body: Column(
-//           children: [
-            
-//             SearchPage2( browseBloc: BlocProvider.of<BrowseBloc>(context)),
-//             Divider(
-//               height: 1, // Height of the divider line
-//               color: Colors.grey[300], // Color of the divider line
-//             ),
-//             Expanded(
-//               child: BlocBuilder<BrowseBloc, BrowseState>(
-//                 builder: (context, state) {
-//                   if (state is RestaurantsLoadInProgress) {
-//                     return const Center(child: CircularProgressIndicator());
-//                   } else if (state is RestaurantsLoadSuccess) {
-//                     return ListView.builder(
-//                       itemCount: state.restaurants.length,
-//                       itemBuilder: (context, index) {
-//                         return GestureDetector(
-//                           onTap: () {
-//                             // Navigate to another view when the restaurant card is clicked
-//                             Navigator.push(
-//                               context,
-//                               MaterialPageRoute(
-//                                 builder: (context) => SpotDetail(restaurant: state.restaurants[index]),
-//                               ),
-//                             );
-//                           },
-//                           child: RestaurantCard(restaurant: state.restaurants[index]),
-//                         );
-//                       }
-//                     );
-//                   } else if (state is RestaurantsLoadFailure) {
-//                     return const Center(child: Text('Failed to load restaurants'));
-//                   }
-//                   // Si el estado inicial es RestaurantsInitial o cualquier otro estado no esperado
-//                   return const Center(child: Text('Start browsing by applying some filters!'));
-//                 },
-//               ),
-//             ),
-//           ],
-//         ),
-//         bottomNavigationBar: CustomNavigationBar(
-//           selectedIndex: 0, // Set the selected index to 1
-//           onItemTapped: (int index) {
-//             // Handle navigation to different views
-//             if (index == 1) {
-//               Navigator.pushNamed(context, 'package:foodbook_app/presentation/views/restaurant_views/login_view.dart');
-//             } else if (index == 2) {
-//               Navigator.pushNamed(context, '/bookmarks');
-//             }
-//           },
-//         ),
-//       )
-//     );
-//   }
-// }
